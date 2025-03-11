ee1974c6c4d3d9f1f56248bc9cdba54cbf633c86
marouane-sadoune <marouanesadoune@gmail.com>
2025-03-11 23:49:14 +0000

    Update AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,5 +13,21 @@ class AppFixtures extends Fixture
         // $manager->persist($product);
 
         $manager->flush();
+        
     }
+    public function load(ObjectManager $manager): void
+{
+    // Create 20 sample products
+    for ($i = 1; $i <= 20; $i++) {
+        $product = new Product();
+        $product->setName('Product ' . $i);
+        $product->setPrice(mt_rand(10, 100));
+        $product->setDescription('Description for product ' . $i);
+
+        $manager->persist($product);
+    }
+
+    $manager->flush();
 }
+}
+
