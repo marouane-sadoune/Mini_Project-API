a96b1cae9adb2ece30b00be6288cf76e0c4ce24d
marouane-sadoune <marouanesadoune@gmail.com>
2025-03-11 23:52:15 +0000

    Update AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\DataFixtures;
+
+use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -8,25 +11,28 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Create categories
+        $categories = [];
+        for ($i = 1; $i <= 5; $i++) {
+            $category = new Category();
+            $category->setName('Category ' . $i);
+            $manager->persist($category);
+            $categories[] = $category; // Store the category for later use
+        }
+
+        // Create products and assign them to categories
+        for ($i = 1; $i <= 20; $i++) {
+            $product = new Product();
+            $product->setName('Product ' . $i);
+            $product->setPrice(mt_rand(10, 100));
+            $product->setDescription('Description for product ' . $i);
+
+            // Assign a random category to the product
+            $product->setCategory($categories[array_rand($categories)]);
+
+            $manager->persist($product);
+        }
 
         $manager->flush();
-        
     }
-    public function load(ObjectManager $manager): void
-{
-    // Create 20 sample products
-    for ($i = 1; $i <= 20; $i++) {
-        $product = new Product();
-        $product->setName('Product ' . $i);
-        $product->setPrice(mt_rand(10, 100));
-        $product->setDescription('Description for product ' . $i);
-
-        $manager->persist($product);
-    }
-
-    $manager->flush();
 }
-}
-
